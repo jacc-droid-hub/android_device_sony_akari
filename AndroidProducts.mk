2aa23754b176f97aba33d5cc27af68eedf10f867
fun guy <39643236+AM4Ryzen@users.noreply.github.com>
2025-03-29 00:19:12 -0500

    Adjust for building RR Q

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_akari.mk
+    $(LOCAL_DIR)/rr_akari.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_akari-user \
-    lineage_akari-userdebug \
-    lineage_akari-eng
+    rr_akari-user \
+    rr_akari-userdebug \
+    rr_akari-eng
